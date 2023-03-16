a6bf0448a9f87480453fb6e9620dd001ca5393fa
Gabriel Moraes <gmoraes.mat@gmail.com>
2023-03-16 12:34:17 -0300

    Inicio da implatação rota de clientes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
+import express from "express";
+import cors from 'cors';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
+//import
 
+const app = express();
 
+app.use(cors())
+app.use(express.json())
 
+//app.use
+
+const port = process.env.port
+
+app.listen(port, () => console.log(`Server running in port: ${port}`));
---
+++ src/routes/clientesRoute.js
@@ -0,0 +1,10 @@
+import { Route } from "express";
+
+
+const clientesRoute = Router();
+
+clientesRoute.post('/cadastro');
+
+export default clientesRoute;
+
+ 
